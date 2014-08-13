78f7fe9aa43fa032c8fe9992d242da0b3b4fa97c
Greg Besson <gregory.besson@adfab.fr>
2014-08-13 03:26:35 +0200

    Travis

--- tests/config/testing.php
+++ tests/config/testing.php
@@ -1,5 +1,15 @@
 <?php
 return array(
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOSqlite\Driver',
+                'params' => array(
+                    'path'=> ':memory:',
+                )
+            )
+        )
+    ),
     'facebook' => array(
         'fb_appid' => 'xxxxxx',
     )
